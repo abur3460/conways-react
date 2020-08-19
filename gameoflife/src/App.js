9f871c81194054fd7a171a970e171d6e6b641f62
Austin Burton <abur3460@gmail.com>
2020-08-18 23:10:30 -0400

    finish basic file structure, begin working on game logic

---
+++ gameoflife/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import Game from "./containers/Game.js";
+
+class App extends Component {
+  render() {
+    return <Game className="game" />;
+  }
+}
+
+export default App;
